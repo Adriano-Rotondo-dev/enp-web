70b6cd87dedc7d4d031dc559893a7946a4a99afc
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2026-02-19 03:25:38 +0100

    Add footer component, enhance routing with view transitions, and update event pages

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,37 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {NavbarComponent} from './components/navbar/navbar'
+import { Component, signal, inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
+import {filter} from 'rxjs/operators' //filtro per eventi router
+import { NavbarComponent } from './components/navbar/navbar'; // Assicurati che il file sia navbar.ts
+import { FooterComponent } from './components/footer/footer'; // Assicurati che il file sia footer.ts
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent], //gestione pagine
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('enp-web'); //funzione simile ad uno useState
+  protected readonly title = signal('enp-web'); 
+  private router = inject(Router) //inject del Router
+  private platformId =inject(PLATFORM_ID) //identificazione - siamo sul browser?
+
+  constructor(){
+    //con il constructor ascoltiamo i cambi di route
+    //ScrollToTop
+    this.router.events.pipe(
+      filter(event=>event instanceof NavigationEnd)
+    ).subscribe(()=>{
+      // scroll SOLO su browser
+      if (isPlatformBrowser(this.platformId)){
+        window.scrollTo({
+          // transizione smooth e non scattosa
+          top:0,
+          behavior:'smooth'
+        })
+      }
+    })
+
+  }
 }
--- src/app/pages/archivio-eventi/archivio-eventi.ts
+++ src/app/pages/archivio-eventi/archivio-eventi.ts
@@ -1,11 +1,86 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface EnpEvent {
+  id: number;
+  vol: string;
+  name: string;
+  date: string;
+  description: string;
+  posterUrl: string;
+}
 
 @Component({
   selector: 'app-archivio-eventi',
-  imports: [],
-  templateUrl: './archivio-eventi.html',
-  styleUrl: './archivio-eventi.css',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './archivio-eventi.html'
 })
 export class ArchivioEventiComponent {
-
+  events = signal<EnpEvent[]>([  
+      {
+      id: 8,
+      vol: 'VOL. 7',
+      name: 'NIGHT OF XMAS',
+      date: '19 DIC',
+      description: 'Cold weather for REFRAINED.',
+      posterUrl: '/poster_placeholder.webp'
+    },
+  {
+      id: 7,
+      vol: 'VOL. 6',
+      name: 'NIGHT OF HALLOWEEN 2.0',
+      date: '25 OTT',
+      description: 'This is Halloween, everybody make a SCREAM with Nihil',
+      posterUrl: '/poster_placeholder.webp'
+    },
+      {
+      id: 6,
+      vol: 'VOL. 5',
+      name: 'NIGHT OF COMICON',
+      date: '14 SET',
+      description: 'Expanding our influence. Yuriko Tiger Dj',
+      posterUrl: '/poster_placeholder.webp'
+    },
+      {
+      id: 5,
+      vol: 'VOL. 4',
+      name: 'NIGHT OF MAKERS',
+      date: '10 MAG',
+      description: 'MEET MY MAKER bring Tesla in the Mosh',
+      posterUrl: '/poster_placeholder.webp'
+    },
+      {
+      id: 4,
+      vol: 'VOL. 3',
+      name: 'NIGHT OF DIAMONDS',
+      date: '22 MAR',
+      description: 'XDIEMONDX was here',
+      posterUrl: '/poster_placeholder.webp'
+    },
+    {    
+      id: 3,
+      vol: 'VOL. 2',
+      name: 'NIGHT OF HALLOWEEN',
+      date: '31 OTT',
+      description: 'This Halloween is Ours',
+      posterUrl: '/poster_placeholder.webp'
+    },
+    {
+      id: 2,
+      vol: 'VOL. 1',
+      name: 'NIGHT OF REBELLION',
+      date: '14 SET',
+      description: 'Notte di fuoco e lacrime. La famiglia cresce.',
+      posterUrl: '/poster_placeholder.webp'
+    },
+    {
+      id: 1,
+      vol: 'VOL. 0',
+      name: 'THE BEGINNING',
+      date: '29 GIU',
+      description: 'Dove tutto è iniziato. Un piccolo locale pieno di emozioni.',
+      posterUrl: '/poster_placeholder.webp'
+    }
+  ]);
 }
