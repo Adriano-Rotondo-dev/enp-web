c9af743360c1666f992d5e69870576b8b178cab7
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2026-02-23 17:15:27 +0100

    feat: Update navbar links and improve menu functionality; enhance chi-siamo page styles

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,17 +1,31 @@
 import { Component, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterLinkActive],
+  imports: [ RouterLink, RouterLinkActive],
   templateUrl: './navbar.html'
 })
 export class NavbarComponent {
+  // Menu State
   isMenuOpen = signal(false);
+
+  // Link dinamici 
+  navLinks = [
+    { path: '/', label: 'Home'},
+    { path: '/prossimo-evento', label: 'Next Night' },
+    { path: '/archivio-eventi', label: 'Past Nights' },
+    { path: '/archivio-foto', label: 'Gallery' },
+    { path: '/chi-siamo', label: 'Crew' }
+  ];
 
   toggleMenu() {
     this.isMenuOpen.update(val => !val);
   }
+
+  closeMenu() {
+    this.isMenuOpen.set(false);
+}
 }
