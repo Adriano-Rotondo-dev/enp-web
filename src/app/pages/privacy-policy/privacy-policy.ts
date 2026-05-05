6f57eeea21699c48cc84ab1bab84e46c8ed189ec
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2026-05-05 10:26:33 +0200

    fix: add robots meta tag to privacy policy component for SEO

--- src/app/pages/privacy-policy/privacy-policy.ts
+++ src/app/pages/privacy-policy/privacy-policy.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
-
+import { Meta } from '@angular/platform-browser'
+ 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
   imports: [],
   templateUrl: './privacy-policy.html'
 })
-export class PrivacyPolicyComponent {}
+export class PrivacyPolicyComponent {
+  constructor(private meta: Meta){
+this.meta.addTag({ name: 'robots', content: 'noindex, nofollow' })
+}
+}
